8cb66ae01ec1d2cb31f751f2d4df48b71a1416c9
azaza <angela.zaza18@gmail.com>
2023-10-24 11:31:50 +0200

    crab config files

---
+++ Utilities/crab/crabConfig_TEMPLATE_Data.py
@@ -0,0 +1,24 @@
+from CRABClient.UserUtilities import config, getUsername
+config = config()
+
+config.General.requestName = 'OUTFILENAME'
+config.General.workArea = 'resultsAna_JOBTAG/'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/afs/cern.ch/work/a/azaza/HccAna/CMSSW_13_0_10/src/Hcc/HccAna/python/templateData_2023EraC.py'
+#config.JobType.outputFiles = ['OUTFILENAME.root']
+#config.JobType.scriptExe = 'submitFileCrab.sh'
+config.Data.inputDBS = 'global'
+config.Data.inputDataset = 'DATASETNAME'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 3
+config.JobType.numCores = 2
+config.Data.publication = True
+# This string is used to construct the output dataset name
+#config.Data.outputDatasetTag = 'QCDpreEE_PT120to170_ntuple'
+
+config.Site.storageSite = 'T2_IT_Bari'
+#config.JobType.allowUndistributedCMSSW = True
+#config.Site.ignoreGlobalBlacklist  = True
