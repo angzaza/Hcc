5b93b3873adabba59eb38ad50dd9cbf5e4036482
azaza <angela.zaza18@gmail.com>
2023-11-13 12:26:34 +0100

    upload updated templates

--- HccAna/python/templateMC_QCDZqq_Summer23.py
+++ HccAna/python/templateMC_QCDZqq_Summer23.py
@@ -4,6 +4,10 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 
 process = cms.Process("HccAnalysis")
 
+from PhysicsTools.PatAlgos.tools.helpers import getPatAlgosToolsTask
+patAlgosToolsTask = getPatAlgosToolsTask(process)
+
+
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 #process.MessageLogger.categories.append('HccAna')
@@ -14,8 +18,7 @@ process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.StandardSequences.Services_cff')
 #process.GlobalTag.globaltag='102X_upgrade2018_realistic_v15'
 #process.GlobalTag.globaltag='102X_upgrade2018_realistic_v18'
-process.GlobalTag.globaltag='130X_dataRun3_PromptAnalysis_v1' 
-
+process.GlobalTag.globaltag='130X_mcRun3_2023_realistic_v14' ### comment if you apply JECs
 
 ### uncomment if you apply JECS ###########################
 #from Configuration.AlCa.GlobalTag import GlobalTag
@@ -23,7 +26,6 @@ process.GlobalTag.globaltag='130X_dataRun3_PromptAnalysis_v1'
 ## se vuoi prendere le JEC dalla GlobalTag, devi utilizzare una GT che le contenga
 ## altrimenti puoi usare una GT che non le contiene e prendere le JEC dal dB file con PoolDBESSource
 ## in questo caso, commenta le due righe sopra legate alla GT e usa la forma sopra process.GlobalTag.globaltag='126X_mcRun3_2023_forPU65_v4'
-
 
 process.Timing = cms.Service("Timing",
                              summaryOnly = cms.untracked.bool(True)
@@ -50,8 +52,8 @@ process.printTree = cms.EDAnalyzer("ParticleListDrawer",
 
 myfilelist = cms.untracked.vstring(
 #''/store/mc/Run3Summer22MiniAODv3/QCD_PT-120to170_TuneCP5_13p6TeV_pythia8/MINIAODSIM/124X_mcRun3_2022_realistic_v12-v2/80000/06ab5fe5-59c7-4dad-9dc2-fe0b75af4379.root',',
-'/store/data/Run2023C/JetMET1/MINIAOD/22Sep2023_v4-v1/30000/0089b0d8-148b-41bc-b3d0-256d1384f6b1.root',
-#'/store/data/Run2023C/JetMET0/MINIAOD/PromptReco-v2/000/367/516/00000/0a71927c-d393-467e-85b7-160f0fc95d5c.root',
+#'/store/mc/Run3Winter23MiniAOD/VBFHto2C_M-125_TuneCP5_13p6TeV_powheg-pythia8/MINIAODSIM/GTv3Digi_GTv3_MiniGTv3_126X_mcRun3_2023_forPU65_v3-v2/2810000/11d4a49f-0565-4dad-a0f0-b0d287889dcc.root',
+'/store/mc/Run3Summer23MiniAODv4/Zto2Q-4Jets_HT-200to400_TuneCP5_13p6TeV_madgraphMLM-pythia8/MINIAODSIM/130X_mcRun3_2023_realistic_v14-v1/30000/011d1978-b670-4faa-b776-37783cc8f7f7.root',
 )
 
 
@@ -62,25 +64,24 @@ process.source = cms.Source("PoolSource",fileNames = myfilelist,
 
 process.TFileService = cms.Service("TFileService",
                                    #fileName = cms.string("prova.root',")
-                                   fileName = cms.string("out_JetMET_2023C_22Sep2023.root")
+                                   fileName = cms.string("out_QCDZqq_Summer23.root")
 )
 
 # jet energy corrections
 
-#import os
+import os
 #from CondCore.DBCommon.CondDBSetup_cfi import *
 #process.jec = cms.ESSource("PoolDBESSource",
 #                           CondDBSetup,
-#                           connect = cms.string("sqlite_file:/afs/cern.ch/work/a/azaza/HccAna/prova/CMSSW_13_1_0_pre2/src/Hcc/HccAna/python/Winter23Prompt23_RunA_V1_DATA.db"),
+#                           connect = cms.string("sqlite_file:/afs/cern.ch/work/a/azaza/HccAna/prova_SegmFault/CMSSW_13_0_13/src/Hcc/HccAna/python/Winter23Prompt23_V2_MC.db"),
 #                           toGet =  cms.VPSet(
 #                              cms.PSet(
 #                                 record = cms.string("JetCorrectionsRecord"),
-#                                 tag = cms.string("JetCorrectorParametersCollection_Winter23Prompt23_RunA_V1_DATA_AK4PFPuppi"),
+#                                 tag = cms.string("JetCorrectorParametersCollection_Winter23Prompt23_V2_MC_AK4PFPuppi"),
 #                                 label= cms.untracked.string("AK4PFPuppi")
 #                              ),
 #              )
 #)
-
 
 from PhysicsTools.PatAlgos.tools.jetTools import updateJetCollection
 
@@ -93,12 +94,29 @@ updateJetCollection(
 
 ## la collezione di jet con le correzioni applicate si chiama "updatedPatJetsUpdatedJEC"
 
-process.jecSequence = cms.Sequence(process.patJetCorrFactorsUpdatedJEC * process.updatedPatJetsUpdatedJEC)
+process.jecSequence = cms.Sequence(process.patJetCorrFactorsUpdatedJEC * process.updatedPatJetsUpdatedJEC) 
+
+########################## JET SMEARING
+process.slimmedJetsSmeared = cms.EDProducer('SmearedPATJetProducer',
+        src = cms.InputTag('updatedPatJetsUpdatedJEC'),
+        enabled = cms.bool(True),
+        rho = cms.InputTag("fixedGridRhoFastjetAll"),
+        algo = cms.string('AK4PFPuppi'),
+        algopt = cms.string('AK4PFPuppi_pt'),
+
+        genJets = cms.InputTag('slimmedGenJets'),
+        dRMax = cms.double(0.2),
+        dPtMaxFactor = cms.double(3),
+
+        debug = cms.untracked.bool(False),
+        variation = cms.int32(0),  # If not specified, default to 0
+)
+#patAlgosToolsTask.add(process.slimmedJetsSmeared)
 
 ###########################   PuJetID-Update
 from RecoJets.JetProducers.PileupJetID_cfi import pileupJetId
 process.pileupJetIdUpdated = pileupJetId.clone(
-  jets=cms.InputTag("updatedPatJetsUpdatedJEC"),
+  jets=cms.InputTag("slimmedJetsSmeared"),
   inputIsCorrected=True,
   applyJec=False,
   vertexes=cms.InputTag("offlineSlimmedPrimaryVertices")
@@ -130,15 +148,12 @@ updateJetCollection(
 process.updatedPatJetsQGT.userData.userFloats.src = ['QGTagger:qgLikelihood']
 
 
-
-
 # Recompute MET
 from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
 
 runMetCorAndUncFromMiniAOD(process,
-            isData=True,
+            isData=False,
             )
-
 
 process.unpackedPatTrigger = cms.EDProducer("PATTriggerObjectStandAloneUnpacker",
     patTriggerObjectsStandAlone = cms.InputTag( 'slimmedPatTrigger' ),
@@ -159,8 +174,9 @@ process.Ana = cms.EDAnalyzer('HccAna',
                               #tauSrc      = cms.untracked.InputTag("slimmedTaus"),
                               #jetSrc       = cms.untracked.InputTag("slimmedJets"),
                               #AK4PuppiJetSrc       = cms.InputTag("updatedPatJetsUpdatedJEC"),
-                              #AK4PuppiJetSrc       = cms.InputTag("slimmedJetsPuppi"),
                               AK4PuppiJetSrc       = cms.InputTag("updatedPatJetsQGT"),
+                              #AK4PuppiJetSrc       = cms.InputTag("updatedPatJetsPileupJetID"),
+                              #AK4PuppiJetSrc       = cms.InputTag("slimmedJetsPuppi"),
 			                  AK8PuppiJetSrc       = cms.untracked.InputTag("slimmedJetsAK8"),
                               #hltPFJetForBtagSrc  = cms.InputTag("hltPFJetForBtag", "", "HLT"),
                               #hltAK4PFJetsCorrectedSrc  = cms.InputTag("hltAK4PFJetsCorrected", "", "HLT"),
@@ -173,18 +189,18 @@ process.Ana = cms.EDAnalyzer('HccAna',
                               #pfJetTagCollectionParticleNetprobtauhSrc = cms.InputTag("hltParticleNetONNXJetTags","probtauh","HLT"),
                               #jetSrc       = cms.untracked.InputTag("slimmedJets"),
                               #mergedjetSrc = cms.untracked.InputTag("corrJets"),
-                              bxvCaloJetSrc =  cms.InputTag("caloStage2Digis","Jet"),
-                              bxvCaloMuonSrc =  cms.InputTag("gmtStage2Digis","Muon"),
-                              bxvCaloHTSrc =  cms.InputTag("caloStage2Digis","EtSum"),
+                              #bxvCaloJetSrc =  cms.InputTag("caloStage2Digis","Jet"),
+                              #bxvCaloMuonSrc =  cms.InputTag("gmtStage2Digis","Muon"),
+                              #bxvCaloHTSrc =  cms.InputTag("caloStage2Digis","EtSum"),
                               #mergedjetSrc = cms.untracked.InputTag("slimmedJets"),
-                              metSrc       = cms.untracked.InputTag("slimmedMETsPuppi"),
+                              #metSrc       = cms.untracked.InputTag("slimmedMETs","","HccAnalysis"),
                               #metSrc       = cms.untracked.InputTag("slimmedMETs","","Hcc"),
-                              #metSrc       = cms.untracked.InputTag("slimmedMETs"),
+                              metSrc       = cms.untracked.InputTag("slimmedMETsPuppi"),
                               vertexSrc    = cms.untracked.InputTag("offlineSlimmedPrimaryVertices"),
                               beamSpotSrc  = cms.untracked.InputTag("offlineBeamSpot"),
                               conversionSrc  = cms.untracked.InputTag("reducedEgamma","reducedConversions"),
-                              isMC         = cms.untracked.bool(False),
-                              isHcc         = cms.untracked.bool(True),
+                              isMC         = cms.untracked.bool(True),
+                              isHcc         = cms.untracked.bool(False),
                               isZqq         = cms.untracked.bool(False),
                               isZcc         = cms.untracked.bool(False),
                               isZbb         = cms.untracked.bool(False),
@@ -211,7 +227,7 @@ process.Ana = cms.EDAnalyzer('HccAna',
                               triggerObjects = cms.InputTag("unpackedPatTrigger"),
                               doJER = cms.untracked.bool(True),
                               doJEC = cms.untracked.bool(True),
-                              algInputTag = cms.InputTag("gtStage2Digis"),
+                              #algInputTag = cms.InputTag("gtStage2Digis"),
                               doTriggerMatching = cms.untracked.bool(False),
                               triggerList = cms.untracked.vstring(
                                 #VBFHToCC
@@ -219,14 +235,6 @@ process.Ana = cms.EDAnalyzer('HccAna',
                                   'HLT_QuadPFJet100_88_70_30_v',
                                   'HLT_QuadPFJet105_88_75_30_PNet1CvsAll0p5_VBF3Tight_v',
                                   'HLT_QuadPFJet105_88_75_30_v',
-                                  'HLT_QuadPFJet111_90_80_30_PNet1CvsAll0p6_VBF3Tight_v',
-                                  'HLT_QuadPFJet111_90_80_30_v',
-                                  'HLT_PFJet80_v',
-                                  'HLT_PFJet60_v',
-                                # Control path VBFHToBB 
-                                  'HLT_QuadPFJet103_88_75_15_v',
-                                  'HLT_QuadPFJet105_88_76_15_v',
-                                  'HLT_QuadPFJet111_90_80_15_v',
                                   'HLT PFJet80_v',
                                   # Single Jet VBFHToBB path:
                                   'HLT_QuadPFJet103_88_75_15_PFBTagDeepJet_1p3_VBF2_v',
@@ -252,7 +260,7 @@ process.Ana = cms.EDAnalyzer('HccAna',
                               year = cms.untracked.int32(2023),####for year put 2016,2017, or 2018 to select correct setting
                               isCode4l = cms.untracked.bool(True), 
 
-payload = cms.string("AK4PFchs"),
+payload = cms.string("AK4PFPuppi"),
 
 
                              )
