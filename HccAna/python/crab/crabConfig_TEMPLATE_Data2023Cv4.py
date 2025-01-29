fa9fb67123c28e96d44d8511c48309a34998200c
azaza <angela.zaza18@gmail.com>
2025-01-29 10:30:00 +0100

    update crab config files

---
+++ HccAna/python/crab/crabConfig_TEMPLATE_Data2023Cv4.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config, getUsername
+config = config()
+
+config.General.requestName = 'OUTFILENAME'
+config.General.workArea = 'resultsAna_JOBTAG/'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/afs/cern.ch/work/a/azaza/HccAna/CMSSW_13_0_13/src/Hcc/HccAna/python/templateData_Prompt2023Cv4_JECdB.py'
+#config.JobType.outputFiles = ['OUTFILENAME.root']
+#config.JobType.scriptExe = 'submitFileCrab.sh'
+config.Data.inputDBS = 'global'
+config.Data.inputDataset = 'DATASETNAME'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 3
+config.Data.lumiMask = '/afs/cern.ch/work/a/azaza/HccAna/CMSSW_13_0_13/src/Hcc/Utilities/crab/Cert_Collisions2023_366442_370790_Golden.json'
+config.JobType.numCores = 2
+config.Data.publication = True
+# This string is used to construct the output dataset name
+#config.Data.outputDatasetTag = 'QCDpreEE_PT120to170_ntuple'
+
+config.Site.storageSite = 'T2_IT_Bari'
+#config.JobType.allowUndistributedCMSSW = True
+#config.Site.ignoreGlobalBlacklist  = True
